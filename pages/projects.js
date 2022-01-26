2b5f686329b8aeaf5b049e75cd354e72851a151c
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-26 16:08:58 -0500

    Project Page WIP

---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+import Container from "../components/Container";
+import Projects from "../components/Projects";
+import React from "react";
+
+export default function projects() {
+  return (
+    <Container>
+      <Projects />
+    </Container>
+  );
+}
