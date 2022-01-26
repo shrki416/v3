23d2e044ccb1b72fa6e3b0bc194db12ffe2d56b6
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-26 16:08:45 -0500

    About page WIP

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import About from "../components/About";
+import Container from "../components/Container";
+
+export default function about() {
+  return (
+    <Container>
+      <About />
+    </Container>
+  );
+}
