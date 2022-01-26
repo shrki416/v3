00c96a2a5f8d5a244545a2d669bf9ed268e566e8
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-26 16:07:21 -0500

    Navbar finished

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 import Link from "next/link";
+import NavArrow from "./NavArrow";
+import ToggleButton from "../components/ToggleButton";
+import data from "../data";
 import { useRouter } from "next/router";
 import { useTheme } from "next-themes";
 
 export default function Navbar() {
   const router = useRouter();
-  console.log(router.asPath);
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
@@ -15,41 +17,138 @@ export default function Navbar() {
   }, []);
 
   return (
-    <div className="max-w-6xl  mx-auto px-4 py-10 md:py-20">
-      <div className="flex  md:flex-row justify-between items-center">
-        <button
-          aria-label="Toggle Dark Mode"
-          type="button"
-          className="w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex flex-end items-center justify-center hover:ring-2 ring-gray-300 transition-all"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          {mounted && (
+    <>
+      <div className="flex md:flex-row justify-between items-center">
+        <div className="flex flex-col">
+          <Link href="/">
+            <a>
+              <h1 className="font-semibold md:text-xl">{data.name}</h1>
+              <p className="text-base font-light text-grey-500">
+                {data.designation}
+              </p>
+            </a>
+          </Link>
+        </div>
+
+        <div className="space-x-8 md:block hidden">
+          <Link href="/about">
+            <a
+              className={`text-base ${
+                router.asPath === "/about"
+                  ? "text-gray-800 font-bold"
+                  : "text-gray-600"
+              }`}
+            >
+              About {router.asPath === "/about" && <NavArrow />}
+            </a>
+          </Link>
+
+          <Link href="/projects">
+            <a
+              className={`text-base  ${
+                router.asPath === "/projects"
+                  ? "text-gray-800 font-bold dark:text-gray-400"
+                  : "text-gray-600 dark:text-gray-300 font-normal "
+              }`}
+            >
+              Projects
+              {router.asPath === "/projects" && <NavArrow />}
+            </a>
+          </Link>
+
+          <Link href="/contact">
+            <a
+              className={`text-base  ${
+                router.asPath === "/contact"
+                  ? "text-gray-800 font-bold dark:text-gray-400"
+                  : "text-gray-600 dark:text-gray-300 font-normal "
+              }`}
+            >
+              Contact
+              {router.asPath === "/contact" && <NavArrow />}
+            </a>
+          </Link>
+        </div>
+
+        <div className="space-x-4 flex flex-row items-center">
+          <a
+            href={data.socialLinks.github}
+            className="text-base font-normal text-gray-500"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
+              width="16"
+              height="16"
               fill="currentColor"
-              stroke="currentColor"
-              className="w-5 h-5 text-gray-800 dark:text-gray-200"
+              viewBox="0 0 24 24"
+              className="bi bi-github h-5 w-5"
             >
-              {theme === "dark" ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              )}
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
             </svg>
-          )}
-        </button>
+          </a>
+
+          <a
+            href={data.socialLinks.linkedin}
+            className="text-base font-normal text-gray-500"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-linkedin h-5 w-5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+            </svg>
+          </a>
+
+          <a
+            href={data.socialLinks.twitter}
+            className="text-base font-normal text-gray-500"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-twitter h-5 w-5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+            </svg>
+          </a>
+
+          <ToggleButton
+            mounted={mounted}
+            setMounted={setMounted}
+            theme={theme}
+            setTheme={setTheme}
+          />
+        </div>
       </div>
-    </div>
+      <div className="space-x-8 block md:hidden mt-4">
+        <Link href="/about">
+          <a className="text-base font-normal">
+            About
+          </a>
+        </Link>
+        <Link href="/projects">
+          <a className="text-base font-normal">
+            Projects
+          </a>
+        </Link>
+        <Link href="/experience">
+          <a className="text-base font-normal">
+            Experience
+          </a>
+        </Link>
+        <Link href="/contact">
+          <a className="text-base font-normal">
+            Contact
+          </a>
+        </Link>
+      </div>
+    </>
   );
 }
