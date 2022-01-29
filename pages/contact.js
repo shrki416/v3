8c089d060c783450dd3450337a108faf54e89d6b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-29 16:26:51 -0500

    Data is coming from CMS

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,44 @@
+import { GraphQLClient, gql, request } from "graphql-request";
+
 import Contact from "../components/Contact";
 import Container from "../components/Container";
 
-export default function contact() {
+const query = gql`
+  query {
+    allGenerals {
+      phone
+      email
+      address
+    }
+    allSocials {
+      title
+      url
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const endpoint = `https://graphql.datocms.com/`;
+
+  const headers = {
+    Authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`,
+  }
+
+  const client = new GraphQLClient(endpoint, { headers });
+
+  const data = await client.request(query);
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function contact({ data }) {
   return (
     <Container>
-      <Contact />
+      <Contact data={data} />
     </Container>
   );
 }
