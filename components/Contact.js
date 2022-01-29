69fbed125bac25143f99d6ab1d78f787e6bab4bb
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-29 16:25:42 -0500

    Data is coming from CMS

--- components/Contact.js
+++ components/Contact.js
@@ -1,9 +1,12 @@
-import GithubSVG from '../components/SVGs/GithubSVG';
-import LinkedInSVG from '../components/SVGs/LinkedInSVG';
-import TwitterSVG from '../components/SVGs/TwitterSVG';
-import data from '../data';
+import GithubSVG from "../components/SVGs/GithubSVG";
+import LinkedInSVG from "../components/SVGs/LinkedInSVG";
+import TwitterSVG from "../components/SVGs/TwitterSVG";
+import data from "../data";
 
-export default function Contact() {
+export default function Contact({ data }) {
+  const { allSocials, allGenerals } = data;
+  const { phone, email, address } = allGenerals[0];
+
   return (
     <section>
       <div className="antialiased">
@@ -26,7 +29,6 @@ export default function Contact() {
             <div className="icons-container inline-flex flex-col my-20">
               <div className="flex flex-row items-center space-x-6 rounded-md border border-[#3c6382] hover:border hover:border-blue-500 p-4">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="currentColor"
@@ -38,11 +40,10 @@ export default function Contact() {
                     d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
                   />
                 </svg>
-                <p className="text-gray-50 font-light text-sm">{data.phone}</p>
+                <p className="text-gray-50 font-light text-sm">{phone}</p>
               </div>
               <div className="flex flex-row items-center space-x-6 rounded-md border border-[#3c6382] hover:border hover:border-blue-500 p-4">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="currentColor"
@@ -51,11 +52,10 @@ export default function Contact() {
                 >
                   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
                 </svg>
-                <p className="text-gray-50 font-light text-sm">{data.email}</p>
+                <p className="text-gray-50 font-light text-sm">{email}</p>
               </div>
               <div className="flex flex-row items-center space-x-6 rounded-md border border-[#3c6382] hover:border hover:border-blue-500 p-4">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="currentColor"
@@ -64,28 +64,17 @@ export default function Contact() {
                 >
                   <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z" />
                 </svg>
-                <p className="text-gray-50 font-light text-sm">
-                  {data.address}
-                </p>
+                <p className="text-gray-50 font-light text-sm">{address}</p>
               </div>
             </div>
             <div className="social-icons flex flex-row space-x-8">
-              <a
-                href={data.socialLinks.github}
-                className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
-              >
+              <a href={allSocials[2].url} className="social-icon">
                 <GithubSVG />
               </a>
-              <a
-                href={data.socialLinks.twitter}
-                className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
-              >
+              <a href={allSocials[0].url} className="social-icon">
                 <TwitterSVG />
               </a>
-              <a
-                href={data.socialLinks.linkedin}
-                className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
-              >
+              <a href={allSocials[1].url} className="social-icon">
                 <LinkedInSVG />
               </a>
             </div>
@@ -95,19 +84,11 @@ export default function Contact() {
               {" "}
               Your Name
             </label>
-            <input
-              type="text"
-              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
-              name="name"
-            />
+            <input type="text" className="input" name="name" />
             <label htmlFor="email" className="text-sm text-gray-600 mx-4 mt-4">
               Email
             </label>
-            <input
-              type="text"
-              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
-              name="email"
-            />
+            <input type="text" className="input" name="email" />
             <label
               htmlFor="message"
               className="text-sm text-gray-600 mx-4 mt-4"
@@ -117,7 +98,7 @@ export default function Contact() {
             <textarea
               rows="4"
               type="text"
-              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              className="input"
               name="message"
             ></textarea>
             <button
