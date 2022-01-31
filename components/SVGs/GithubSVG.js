15267ee31ebbb2498a97ed259fa0047e141437be
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-31 16:26:47 -0500

    Refactor and clean-up

--- components/SVGs/GithubSVG.js
+++ components/SVGs/GithubSVG.js
@@ -1,9 +1,6 @@
 export default function GithubSVGIcon() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
       fill="currentColor"
       viewBox="0 0 24 24"
       className="bi bi-github h-5 w-5"
--- components/SVGs/TwitterSVG.js
+++ components/SVGs/TwitterSVG.js
@@ -1,9 +1,6 @@
 export default function TwitterSVG() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
       fill="currentColor"
       className="bi bi-twitter h-5 w-5"
       viewBox="0 0 16 16"
