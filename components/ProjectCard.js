51adbac424f7fd6379c64016ed25c52b3e6150d9
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-31 16:24:05 -0500

    Create a projectCard component

---
+++ components/ProjectCard.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function ProjectCard({ title, link, imgUrl, number }) {
+  return (
+    <a href={link} className="w-full block shadow-2xl">
+      <div className="relative overflow-hidden">
+        <div className="h-72 object-cover">
+          <Image
+            src={imgUrl}
+            alt="avatar"
+            layout="fill"
+            className="transform hover:scale-125 transition duration-2000 ease-out object-cover h-full w-full"
+          />
+        </div>
+        <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+          {title}
+        </h1>
+        <h1 className="absolute bottom-10 left-10 text-red-500 font-bold text-xl">
+          {number.length === 1 ? "0" + number : number}
+        </h1>
+      </div>
+    </a>
+  );
+}
