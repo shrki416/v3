bbdbe659898dc3a1fe84872e2bba9f91495694a0
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-31 16:25:32 -0500

    Bring in data via dataCMS

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import ProjectCard from "./ProjectCard";
 import data from "../data";
 
-export default function Projects() {
+export default function Projects({ data }) {
   return (
     <section>
       <div>
@@ -11,13 +12,13 @@ export default function Projects() {
       </div>
       <div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          {data.projects.map((proj, idx) => (
+          {data.map((project, index) => (
             <ProjectCard
-              key={idx}
-              title={proj.title}
-              link={proj.link}
-              imgUrl={proj.imgUrl}
-              number={`${idx + 1}`}
+              key={project.id}
+              title={project.title}
+              link={project.url}
+              imgUrl={project.image.url}
+              number={`${index + 1}`}
             />
           ))}
         </div>
@@ -25,26 +26,3 @@ export default function Projects() {
     </section>
   );
 }
-
-const ProjectCard = ({ title, link, imgUrl, number }) => {
-  return (
-    <a href={link} className="w-full block shadow-2xl">
-      <div className="relative overflow-hidden">
-        <div className="h-72 object-cover">
-          <Image
-            src={imgUrl}
-            alt="avatar"
-            layout="fill"
-            className="transform hover:scale-125 transition duration-2000 ease-out object-cover h-full w-full"
-          />
-        </div>
-        <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-          {title}
-        </h1>
-        <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-          {number.length === 1 ? "0" + number : number}
-        </h1>
-      </div>
-    </a>
-  );
-};
