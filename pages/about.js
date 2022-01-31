894b7c10e2111c01509236e93cb07b1be424be2c
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-31 16:24:58 -0500

    Bring in data via dataCMS

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,60 @@
+import { GraphQLClient, gql, request } from "graphql-request";
+
 import About from "../components/About";
 import Container from "../components/Container";
 
-export default function about() {
+const query = gql`
+  query {
+    allAbouts {
+      title
+      text
+      id
+    }
+    allGenerals {
+      phone
+      name
+      id
+      githubUsername
+      email
+      designation
+      avatarUrl
+      address
+    }
+    allTechStacks {
+      image {
+        url
+        width
+        basename
+        height
+        id
+      }
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const endpoint = `https://graphql.datocms.com/`;
+
+  const headers = {
+    Authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`,
+  };
+
+  const client = new GraphQLClient(endpoint, { headers });
+  const data = await client.request(query);
+
+
+  return {
+    props: {
+      general: data.allGenerals[0],
+      techStacks: data.allTechStacks,
+    },
+  };
+}
+
+export default function about({ general, techStacks }) {
   return (
     <Container>
-      <About />
+      <About general={general} techStacks={techStacks} />
     </Container>
   );
 }
