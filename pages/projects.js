2946352b6581b259d58457e60803b729559752dd
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-31 16:24:49 -0500

    Bring in data via dataCMS

--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,43 @@
+import { GraphQLClient, gql, request } from "graphql-request";
+
 import Container from "../components/Container";
 import Projects from "../components/Projects";
-import React from "react";
 
-export default function projects() {
+const query = gql`
+  query {
+    allProjects {
+      image {
+        basename
+        url
+      }
+      title
+      url
+      id
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const endpoint = `https://graphql.datocms.com/`;
+
+  const headers = {
+    Authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`,
+  };
+
+  const client = new GraphQLClient(endpoint, { headers });
+  const data = await client.request(query);
+
+  return {
+    props: {
+      data: data.allProjects,
+    },
+  };
+}
+
+export default function projects({ data }) {
   return (
     <Container>
-      <Projects />
+      <Projects data={data} />
     </Container>
   );
 }
