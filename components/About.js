2b81c3e8fcedff458257b2d4243fe3d96d35ad60
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-02-05 19:40:17 -0500

    Data is coming from CMS

--- components/About.js
+++ components/About.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import UpArrow from './SVGs/UpArrow';
-import data from "../data";
+import { StructuredText } from "react-datocms";
+import UpArrow from "./SVGs/UpArrow";
 
-export default function About({ general, techStacks }) {
+export default function About({ general, techStacks, socials }) {
   return (
     <section className="max-w-5xl mx-auto">
       <div className="flex justify-between items-center">
@@ -28,7 +28,7 @@ export default function About({ general, techStacks }) {
       </div>
 
       <div className="text-container mx-auto md:pt-20 pb-10 text-gray-700 dark:text-gray-400">
-        <p className="leading-loose text-2xl md:text-4xl">{data.about.title}</p>
+        <p className="leading-loose text-2xl md:text-4xl">{general.title}</p>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-20">
@@ -57,7 +57,7 @@ export default function About({ general, techStacks }) {
               I&apos;m looking for a job currently, if you see me as a good fit,
               check my{" "}
               <a
-                href={data.resumeUrl}
+                href={general.resumeUrl}
                 target="__blank"
                 className="text-green-600 border-b-2 border-green-600 dark:border-green-600 font-bold dark:text-green-600"
               >
@@ -74,12 +74,12 @@ export default function About({ general, techStacks }) {
             <div className="mt-4 ml-4">
               <div className="flex flex-row justify-start items-center">
                 <a
-                  href={data.socialLinks.github}
+                  href={socials[2].url}
                   className="flex flex-row items-center space-x-4 group"
                 >
                   <span className="my-4 dark:text-red-500">&rarr;</span>
                   <p className="text-lg text-gray-700 font-mono relative overflow-hidden dark:text-gray-500">
-                    <span className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></span>
+                    <span className="absolute h-0.5 w-full bg-red-500 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></span>
                     GitHub
                   </p>
                 </a>
@@ -87,12 +87,12 @@ export default function About({ general, techStacks }) {
 
               <div className="flex flex-row justify-start items-center">
                 <a
-                  href={data.socialLinks.linkedin}
+                  href={socials[1].url}
                   className="flex flex-row items-center space-x-4 group"
                 >
                   <span className="my-4 dark:text-red-500">&rarr;</span>
                   <p className="text-lg text-gray-700 font-mono relative overflow-hidden dark:text-gray-500">
-                    <span className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></span>
+                    <span className="absolute h-0.5 w-full bg-red-500 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></span>
                     LinkedIn
                   </p>
                 </a>
@@ -100,12 +100,12 @@ export default function About({ general, techStacks }) {
 
               <div className="flex flex-row justify-start items-center">
                 <a
-                  href={data.socialLinks.twitter}
+                  href={socials[0].url}
                   className="flex flex-row items-center space-x-4 group"
                 >
                   <span className="my-4 dark:text-red-500">&rarr;</span>
                   <p className="text-lg text-gray-700 font-mono relative overflow-hidden dark:text-gray-500">
-                    <span className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></span>
+                    <span className="absolute h-0.5 w-full bg-red-500 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></span>
                     Twitter
                   </p>
                 </a>
@@ -114,15 +114,26 @@ export default function About({ general, techStacks }) {
           </section>
         </div>
         <div className="col-span-1 md:col-span-2">
-          {data.about.description?.map((desc, idx) => (
-            <p
-              key={idx}
-              className="text-xl text-gray-800 mb-4 dark:text-gray-400"
-            >
-              {desc}
+          <div className="text-xl text-gray-800 mb-4 dark:text-gray-400">
+            <p className="mb-5">
+              I&apos;ve been developing Full Stack application for the past 2
+              years. I am currently a support-coach @ Software Development
+              Mastermind.
             </p>
-          ))}
-
+            <p className="mb-5">
+              I&apos;ve been coding in React on the frontend and NodeJS on the
+              backend. I prefer PostgreSQL as a db, but have used others such as
+              MongoDB. When I&apos;m not coding or working on new features, I
+              spend my time improving code quality by refactoring and
+              introducing design patterns I learned about.
+            </p>
+            <p className="mb-5">
+              I have many hobbies; the ones I care about the most are traveling,
+              I LOVE to travel, learn about different cultures and see other
+              places. I enjoy woodworking, I&apos;ve made a few small things such as
+              a coffee table and a small seating bench.
+            </p>
+          </div>
           <h1 className="bg-red-500 text-3xl rounded-md px-2 py-1 inline-block font-bold text-gray-50">
             Tech Stack && Tools
           </h1>
